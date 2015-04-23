0a03d0cc3f5a8484d3d18d0ea40087c9110d15e4
divinites <divinites@gmail.com>
2015-04-23 14:32:09 +0100

    take lib and templates in parts, improve code maintenance.

--- mark.py
+++ mark.py
@@ -1,9 +1,9 @@
 #!/Users/divinites/anaconda/bin/python
 import os
 from argparse import *
-
-from template import *
-from help import help_statement
+import lib.libmark
+import template.templates
+from helpstats import help_statement
 
 
 os.system('rm -rf *.docx')
@@ -12,6 +12,7 @@ parser = ArgumentParser(description=help_statement("usage"))
 parser.add_argument("-i", '--input', help=help_statement('-i'), dest='input', required=True)
 parser.add_argument("-o", '--output', default='./',
                     help=help_statement('-o'), dest='output', required=False)
+parser.add_argument("-t", '--type', help=help_statement('-t'), dest = 'type', required=False)
 
 exclusive_group = parser.add_mutually_exclusive_group()
 exclusive_group.add_argument("-a", "--all", help=help_statement('-a'),
@@ -19,12 +20,23 @@ exclusive_group.add_argument("-a", "--all", help=help_statement('-a'),
 exclusive_group.add_argument("-s", "--student", type=str,
                              help=help_statement('-s'), dest='student_no', required=False)
 args = parser.parse_args()
-marks = Result(args.input)
+marks = lib.libmark.Profiles(args.input)
 score = {}
 forms = {}
+current_form =None
+
+if args.type is None:
+    current_form = template.templates.UGECSForm
+else:
+    if args.type in template.templates.supported_form:
+        constructor = globals()[args.type]
+        current_form = constructor()
+    else:
+        raise(ValueError)
+
 for i in marks.student_list:
     score[i] = marks.round(marks.grading(i))
-    forms[i] = FeedbackForm(args.input, i)
+    forms[i] = current_form(args.input, i)
 
 if not os.path.exists(args.output):
     os.mkdir(args.output)
@@ -32,7 +44,10 @@ if not os.path.exists(args.output):
 if args.all is True:
     for i, form in forms.items():
         form.print_form(args.output)
+
 if args.student_no is not None:
     forms[args.student_no].print_form(args.output)
+
+
 
 
